111e4d181f3d4ae495e9403f12c8cbdc61e43974
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-10-18 10:44:30 +0600

    created mongodb database and make database and collection also added six brand collection for show ui

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5003;
 
-
-
-
-
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://rahimbadsa723:Ofqat5fdGeCj9PDc@cluster0.r2ad8dg.mongodb.net/?retryWrites=true&w=majority";
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -20,16 +19,27 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const brandCollection = client.db('brandNameDB').collection('brandNameDBCollection');
 
-    app.get('/brand', (req, res)=>{
 
-        res.send('working1')
 
-    })
+    app.get('/brand', async (req, res) => {
+        try {
+          const cursor = brandCollection.find();
+          const brands = await cursor.toArray();
+      
+          res.json(brands); // Send the retrieved data as a JSON response
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ error: "Internal Server Error" });
+        }
+      });
+      
 
 
     // Send a ping to confirm a successful connection
